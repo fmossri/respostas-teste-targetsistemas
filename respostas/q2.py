ad2de07c662dd12c049b7f64cbc44184fb443008
fmossri <chicobulhoes@gmail.com>
2024-09-04 23:12:15 -0300

    push

--- respostas/q2.py
+++ respostas/q2.py
@@ -3,11 +3,14 @@ seja maiúscula ou minúscula, além de informar a quantidade de vezes em que el
 '''
 def quantas_letras_a(string):
     counter = 0
-    
+    letras_a = ['a', 'A', 'á', 'Á', 'à', 'À', 'ã', 'Ã', 'â', 'Â']
     for letra in string:
-        if letra == 'a' or letra == 'A':
+        if letra in letras_a:
             counter += 1
     if counter == 0:
         print("A letra 'a' não está presente na string")
     else:
         print(f"A letra 'a' está presente na string {counter} vezes.")
+
+string = "A string em questão possui 4 letras 'A'"
+quantas_letras_a(string)
--- respostas/q5.py
+++ respostas/q5.py
@@ -4,16 +4,15 @@ diferentes. Você não pode ver as lâmpadas da sala em que está, mas pode liga
 os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla 
 qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas 
 das lâmpadas, qual interruptor controla cada lâmpada? 
-'''
 
-'''
-  Primeiro ligo o interruptor 1 e espero alguns minutos para que a lâmpada correspondente 
-esquente. Em seguida aciono o interruptor 2, e vou à sala "A". Se a lâmpada estiver
+Resposta:
+Primeiro ligo o interruptor 1 e espero alguns minutos para que a lâmpada correspondente 
+esquente. Em seguida aciono o interruptor 2 e vou à sala "A". Se a lâmpada estiver
 acesa, ela corresponde ao interruptor 2; se estiver apagada, mas quente, ao 
 interrutpor 1; e se estiver apagada e fria, ao interruptor 3.
-  Com apenas duas correspondências desconhecidas, volto à sala dos interruptores. Se a 
+Com apenas duas correspondências desconhecidas, volto à sala dos interruptores. Se a 
 sala "A" corresponder ao interruptor 2, desligo-o, 
 ligo o interruptor 1 novamente e vou à sala "B". Caso contrário, apenas deixo 
 os interrutptores como estão e vou à sala "B" para verificar se sua lâmpada está acesa.
-  Dessa forma consigo determinar a correspondência entre cada interruptor e cada sala.
+Dessa forma consigo determinar a correspondência entre cada interruptor e cada sala.
 '''
